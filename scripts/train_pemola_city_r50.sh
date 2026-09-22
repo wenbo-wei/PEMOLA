c6fe622c955b263f23e05321b5301470f24b3c30
Wenbo Wei <wenbo.weiwb@gmail.com>
2026-09-22 12:55:25 +0800

    Use 3 GPUs for Cityscapes training with a compatible batch size

--- scripts/train_pemola_city_r50.sh
+++ scripts/train_pemola_city_r50.sh
@@ -3,5 +3,6 @@ cd "$(dirname "$0")/.." || exit 1
 export DETECTRON2_DATASETS=datasets/data
 python ./train_net.py \
 --config-file configs/cityscapes/panoptic-segmentation/maskformer2_R50_bs16_90k.yaml \
---num-gpus 2 \
-MODEL.PEMOLA.PE_MODULATION True
+--num-gpus 3 \
+MODEL.PEMOLA.PE_MODULATION True \
+SOLVER.IMS_PER_BATCH 24
